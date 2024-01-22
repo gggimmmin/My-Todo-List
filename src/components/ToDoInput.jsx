e12cc1a0931e12e9ddec99100290de0b198d7ed6
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-01-23 02:22:26 +0900

    feat: 입력한 값 리스트로 나타내기

--- src/components/ToDoInput.jsx
+++ src/components/ToDoInput.jsx
@@ -1,16 +1,55 @@
+import { useState } from "react";
 import styles from "./ToDoInput.module.css";
 
 const ToDoInput = () => {
+  const [list, setList] = useState([
+    { id: 1, title: "리액트공부", content: "리액트1강듣기" },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const contentChangeHandler = (e) => {
+    setContent(e.target.value);
+  };
+
+  const addToDoHandler = () => {
+    const newList = {
+      id: list.length + 1,
+      title: title,
+      content: content,
+    };
+
+    setList([...list, newList]);
+    setTitle("");
+    setContent("");
+  };
+
   return (
-    <div className={styles.Formcontrols}>
-      <div className={styles.titleContainer}>
-        제목
-        <input />
-        내용
-        <input />
+    <div>
+      <div className={styles.Formcontrols}>
+        <div className={styles.titleContainer}>
+          제목
+          <input type="text" value={title} onChange={titleChangeHandler} />
+          내용
+          <input type="text" value={content} onChange={contentChangeHandler} />
+        </div>
+        <div>
+          <button className={styles.addButton} onClick={addToDoHandler}>
+            추가하기
+          </button>
+        </div>
       </div>
       <div>
-        <button className={styles.addButton}>추가하기</button>
+        {list.map((item) => (
+          <p key={item.id}>
+            {item.title}, {item.content}
+          </p>
+        ))}
       </div>
     </div>
   );
